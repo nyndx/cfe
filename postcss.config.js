9de9c95866bf42e08360730e37ebc3478f7a4f2e
Max Ndundula <nynd_x@pm.me>
2020-05-19 14:14:15 -0400

    changed postcss confi

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,7 @@ const purgecss = [
 module.exports = () => ({
   plugins: [
     require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    // require("autoprefixer"),
+    // ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 })
