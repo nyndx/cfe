edf109aa3bf087c3e4ba0572a18ff91cc3b8979d
Max Ndundula <nynd_x@pm.me>
2020-05-19 21:42:15 -0400

    changed padding on container

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,7 @@ const Layout = ({ children, hidebg = true }) => {
             hidebg ? "hidden" : "block"
           } bg-gray-100 border-b-2 absolute inset-0 h-80vh lg:h-95vh transform skew-y-3 origin-top-right`}
         ></div>
-        <div className="container mx-auto px-8 bg-transparent relative z-1000">
+        <div className="container mx-auto px-4 sm:px-8 bg-transparent relative z-1000">
           <Header />
           {children}
           <Footer />
